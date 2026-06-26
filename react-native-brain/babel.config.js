8a502412da110ef2befaa76d268251ae7ea32263
ztrosenkova-lang <ztrosenkova@gmail.com>
2026-06-27 00:42:51 +0300

    Update babel.config.js

--- react-native-brain/babel.config.js
+++ react-native-brain/babel.config.js
@@ -1,8 +1,3 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
-  plugins: [
-    // Этот плагин принудительно заставляет Babel использовать внешние хелперы,
-    // что полностью блокирует вызов дефолтного хаба и убирает ошибку "Helpers are not supported"!
-    ['@babel/plugin-transform-runtime', { helpers: true }]
-  ]
 };
