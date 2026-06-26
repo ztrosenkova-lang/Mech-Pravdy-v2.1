7e2ae1e6e775f0ccba3ef6ea6d6de4b257875fe7
ztrosenkova-lang <ztrosenkova@gmail.com>
2026-06-27 00:42:16 +0300

    Update metro.config.js

--- react-native-brain/metro.config.js
+++ react-native-brain/metro.config.js
@@ -1,11 +1,16 @@
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const path = require('path');
 
-/**
- * Metro configuration
- * https://reactnative.dev
- *
- * @type {import('metro-config').MetroConfig}
- */
-const config = {};
+const config = {
+  resolver: {
+    extraNodeModules: {
+      '@babel/traverse': path.resolve(__dirname, 'node_modules/@babel/traverse'),
+      '@babel/core': path.resolve(__dirname, 'node_modules/@babel/core'),
+    },
+    blockList: [
+      /node_modules\/@pocketpalai\/llama\.rn\/node_modules\/@babel\/.*/
+    ]
+  },
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
