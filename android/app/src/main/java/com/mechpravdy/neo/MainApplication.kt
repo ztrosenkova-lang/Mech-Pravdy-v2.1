cbaeeb33f484b27004809ba3bb51a8578ecba117
ztrosenkova-lang <ztrosenkova@gmail.com>
2026-07-01 13:51:29 +0300

    Update MainApplication.kt

--- android/app/src/main/java/com/mechpravdy/neo/MainApplication.kt
+++ android/app/src/main/java/com/mechpravdy/neo/MainApplication.kt
@@ -1,7 +1,6 @@
 package com.mechpravdy.neo
 
 import android.app.Application
-import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
@@ -14,19 +13,26 @@ class MainApplication : Application(), ReactApplication {
         override fun getJSMainModuleName(): String = "index"
         override fun getUseDeveloperSupport(): Boolean = false
 
+        // ИСПРАВЛЕНО: Ручная передача пакета PocketPal без CLI-скриптов и PackageList!
         override fun getPackages(): List<ReactPackage> {
-            return PackageList(this).packages
+            return listOf(
+                com.facebook.react.shell.MainReactPackage(),
+                // Вшиваем пакет инференса напрямую из импортированной библиотеки PocketPal
+                com.pocketpalai.llama.LlamaPackage() 
+            )
         }
     }
 
     override fun onCreate() {
         super.onCreate()
+        // Инициализируем SoLoader для загрузки .so библиотек в память процесса
         SoLoader.init(this, false)
         try {
+            // Принудительно разворачиваем таблицы JNI на старте
             reactNativeHost.reactInstanceManager.createReactContextInBackground()
-            android.util.Log.d("MECH_SYSTEM", "🚀 Нативный С++ React-контур запущен")
+            android.util.Log.d("MECH_SYSTEM", "🚀 Нативный С++ мост JNI успешно развернут в памяти.")
         } catch (e: Exception) {
-            android.util.Log.e("MECH_SYSTEM", "❌ Ошибка: ${e.message}")
+            android.util.Log.e("MECH_SYSTEM", "❌ Критический сбой запуска моста: ${e.message}")
         }
     }
 }
