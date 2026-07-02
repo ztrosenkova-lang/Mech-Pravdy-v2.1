e0d05f678d3f372f0af2db349e34c3ecba49eced
ztrosenkova-lang <ztrosenkova@gmail.com>
2026-07-02 05:52:08 +0300

    Update MainApplication.kt

--- android/app/src/main/java/com/mechpravdy/neo/MainApplication.kt
+++ android/app/src/main/java/com/mechpravdy/neo/MainApplication.kt
@@ -1,37 +1,10 @@
 package com.mechpravdy.neo
 
 import android.app.Application
-import com.facebook.react.ReactApplication
-import com.facebook.react.ReactNativeHost
-import com.facebook.react.ReactPackage
-import com.facebook.react.defaults.DefaultReactNativeHost
-import com.facebook.soloader.SoLoader
 
-class MainApplication : Application(), ReactApplication {
-
-    override val reactNativeHost: ReactNativeHost = object : DefaultReactNativeHost(this) {
-        override fun getJSMainModuleName(): String = "index"
-        override fun getUseDeveloperSupport(): Boolean = false
-
-        // ИСПРАВЛЕНО: Вызываем точное фабричное имя класса RNLlamaPackage() из пакета com.rnllama
-        override fun getPackages(): List<ReactPackage> {
-            return listOf(
-                com.facebook.react.shell.MainReactPackage(),
-                com.rnllama.RNLlamaPackage()
-            )
-        }
-    }
-
+class MainApplication : Application() {
     override fun onCreate() {
         super.onCreate()
-        // Инициализируем загрузчик нативных С++ библиотек
-        SoLoader.init(this, false)
-        try {
-            // Разворачиваем С++ таблицы рантайма в памяти процесса
-            reactNativeHost.reactInstanceManager.createReactContextInBackground()
-            android.util.Log.d("MECH_SYSTEM", "🚀 Нативный С++ мост JNI успешно развернут.")
-        } catch (e: Exception) {
-            android.util.Log.e("MECH_SYSTEM", "❌ Критический сбой запуска моста: ${e.message}")
-        }
+        android.util.Log.d("MECH_SYSTEM", "🚀 Чистый нативный старт приложения.")
     }
 }
